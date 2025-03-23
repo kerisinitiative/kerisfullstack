00cf5bf49e7b7ad23b50130ec97a09ca98af353e
Piqim <115794663+piqim@users.noreply.github.com>
2025-03-23 13:53:43 -0700

    + havent added the react pages yet

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,17 +10,117 @@ import RecordList from "./components/RecordList";
 import "./index.scss";
 import Dev from "./components/Dev";
 
+/* Routes (pages) for the webapp */
 const router = createBrowserRouter([
+  /**
+   * client-side
+   **/
   {
     path: "/",
     element: <App />,
     children: [
+      // home page - route
       {
-        path: "/",
+        index: true,
         element: <RecordList />,
+      },
+      // scholar route
+      {
+        path: "scholar",
+        children: [
+          // scholar list
+          {
+            index: true,
+            element: <Dev />,
+          },
+          // scholar detail
+          {
+            path: "detail/:id",
+            element: <Dev />,
+          },
+        ],
+      },
+      // scholarship route
+      {
+        path: "scholarship",
+        children: [
+          // scholarship list
+          {
+            index: true,
+            element: <Dev />,
+          },
+          // scholarship detail
+          {
+            path: "detail/:id",
+            element: <Dev />,
+          },
+        ,]
       },
     ],
   },
+
+  /** 
+   * admin-side
+   **/
+  {
+    path: "/admin",
+    element: <App />,
+    children: [
+      // admin dashboard page
+      {
+        index: true,
+        element: <RecordList />,
+      },
+      // list of scholar record
+      {
+        path: "scholar-list",
+            element: <Dev />,
+      },
+      // add scholar
+      {
+        path: "scholar-list/add",
+            element: <Dev />,
+      },
+      // edit scholar
+      {
+        path: "scholar-list/edit:id",
+            element: <Dev />,
+      },
+      // list of scholarship record
+      {
+        path: "scholarship-list",
+            element: <Dev />,
+      },
+      // add scholarship
+      {
+        path: "scholarship-list/add",
+            element: <Dev />,
+      },
+      // edit scholarship
+      {
+        path: "scholarship-list/edit:id",
+            element: <Dev />,
+      },
+
+    ],
+  },
+
+  /** 
+   * Dev Page
+   **/
+  {
+    path: "/dev",
+    element: <App />,
+    children: [
+      {
+        path: "/dev",
+        element: <Dev />,
+      },
+    ],
+  },
+
+
+  /* Old stuff
   {
     path: "/edit/:id",
     element: <App />,
@@ -41,17 +141,8 @@ const router = createBrowserRouter([
       },
     ],
   },
-  // Dev Page
-  {
-    path: "/dev",
-    element: <App />,
-    children: [
-      {
-        path: "/dev",
-        element: <Dev />,
-      },
-    ],
-  },
+  */
+
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
