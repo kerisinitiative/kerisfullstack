86fd0547172d9bfe556fd9b6636beb6bfcd28722
Piqim <115794663+piqim@users.noreply.github.com>
2025-03-23 13:53:33 -0700

    Added the necessary routes for all the planned pages

--- client/src/components/RecordList.jsx
+++ client/src/components/RecordList.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import profile_pic from "./piqim.jpg";
 
-// your mom
+// your mom mostly
 const Record = (props) => (
   <>
   
